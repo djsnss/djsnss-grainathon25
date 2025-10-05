8b91225c828f26cc689018448704013752ea3fae
djsnss <djnss.official@gmail.com>
2025-10-05 21:57:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import ShootingStars from './Components/ShootingStars';
-import { GrainathonLeaderboard } from './Components/GrainathonLeaderboard';
-import MovingRocket from './Components/MovingRocket';
-import './Styles/App.css';
 
 function App() {
   return (
-    <div className="app-container" style={{ position: 'relative', zIndex: 10 }}>
-      <ShootingStars starCount={30} />
-      <MovingRocket speed={15} />
-      <div className="leaderboard-wrapper" style={{ position: 'relative', zIndex: 10 }}>
-        <GrainathonLeaderboard />
-      </div>
+    <div>
+      
     </div>
   );
 }
