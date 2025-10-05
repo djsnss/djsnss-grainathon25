62086be91c9d96cb3c8c74e8090e938531d4ade9
djsnss <djnss.official@gmail.com>
2025-10-05 18:41:58 +0530

    fonts imported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         'libertinus': ['"Libertinus Keyboard"', 'monospace'],
-//         'raleway': ['Raleway', 'sans-serif'],
-//         'roboto': ['Roboto', 'sans-serif'],
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -28,12 +10,10 @@ module.exports = {
         'libertinus': ['"Libertinus Keyboard"', 'monospace'],
         'raleway': ['Raleway', 'sans-serif'],
         'roboto': ['Roboto', 'sans-serif'],
-      },
-      colors: {
-        skyblue: {
-          500: "#309cc0",
-          600: "#0696c6"
-        },
+        '1up': ['1up', 'sans-serif'],
+        'arcade': ['arcade', 'sans-serif'],
+        'bungee': ['"Bungee Spice"', 'cursive'],
+        'silkscreen': ['Silkscreen', 'sans-serif'],
       },
     },
   },
