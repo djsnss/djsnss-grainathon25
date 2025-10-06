74e0e721569acc36d7d92571f964aaa7e4919079
HP@Swaroop <shindolkarswaroop@gmail.com>
2025-10-06 20:17:27 +0530

    Cards implemented in committee

--- src/Pages/Committee.jsx
+++ src/Pages/Committee.jsx
@@ -5,22 +5,23 @@ import Card from "../Components/Card";
 
 const Committee = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center h-screen w-screen relative overflow-hidden">
       <img
         src="/assets/bg2.jpeg"
         alt="Committee Background"
         className="absolute z-0 top-0 left-0 w-full h-full object-cover"
-        style={{ maxWidth: "100%", maxHeight: "100%" }}
       />
 
       <Header />
 
-      <div className="w-full h-max flex flex-row items-center justify-center gap-10 text-6xl text-white font-arcade font-bold z-10">
-        <h1 >Committee</h1>
-        <h1 >Leaderboard</h1>
+      {/* ↓ Slightly lower than header */}
+      <div className="w-full h-max flex flex-row items-center justify-center gap-10 text-4xl text-white font-up font-bold z-10 mt-10 tracking-wider">
+        <h1>Committee</h1>
+        <h1>Leaderboard</h1>
       </div>
 
-      <div className="z-10 w-[90%] h-[400px] flex flex-col items-center justify-center mt-10 text-white">
+      {/* ↓ 3 cards per row, tight spacing */}
+      <div className="z-10 w-[55%]  gap-2 mt-6 flex flex-wrap justify-between items-center text-white">
         {comm.map((item, index) => (
           <Card key={index} item={item} />
         ))}
